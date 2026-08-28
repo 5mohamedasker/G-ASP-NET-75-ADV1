ccd5a148bd9f7695070a7ccb5415ccdbe9f23b57
5mohamedasker <5mohamedasker@gmail.com>
2026-08-28 10:47:06 +0300

    Question01

--- Advanced_C#_Assignment01/Program.cs
+++ Advanced_C#_Assignment01/Program.cs
@@ -4,6 +4,16 @@
     {
         static void Main()
         {
+            #region Question01
+            /* What is a generic class? Why use generics?
+                ==>> Class that can work with different data types
+                     and the data type is specified when we create an object of the clas
+                ==>>We use it because it can work with different data types
+                    when we have the same behavior or implementation
+                    but need to work with different data types
+                    which improves code reusability and type safety
+            */
+            #endregion
         }
     }
 }
